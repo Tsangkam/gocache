36e62ffe9e99f10b6a65a80a2254959caa8d73ea
Tsangkam <1041122067@qq.com>
2022-10-02 16:06:37 +0000

    first commit: lru and apart of cache

---
+++ README.md
@@ -0,0 +1,2 @@
+# gocache
+A Distributed cache system by go, from https://geektutu.com/post/geecache.html
---
+++ gocache/gocache_test.go
@@ -0,0 +1,44 @@
+package gocache
+
+import (
+	"testing"
+)
+
+func addSth(testCache *cache, key string, value string) {
+	testValue := ByteView{
+		b: []byte(value),
+	}
+	testCache.add(key, testValue)
+}
+
+func TestAdd(t *testing.T) {
+	testCache := cache{}
+	for i := 0; i < 5; i++ {
+		letter := string(rune(int('a') + i))
+		// go addSth(&testCache, letter, "test_"+letter)
+		addSth(&testCache, letter, "test_"+letter)
+	}
+}
+
+func TestGet(t *testing.T) {
+	testCache := cache{}
+	for i := 0; i < 5; i++ {
+		letter := string(rune(int('a') + i))
+		// go addSth(&testCache, letter, "test_"+letter)
+		addSth(&testCache, letter, "test_"+letter)
+	}
+
+	for i := 0; i < 5; i++ {
+		key := string(rune(int('a') + i))
+		value, ok := testCache.get(key)
+		if !ok || string(value.b) != "test_"+key {
+			t.Fatalf("get no key " + key)
+		}
+	}
+
+	//test key not exist conditions
+	_, ok := testCache.get("key_not_exist")
+	if ok {
+		t.Fatalf("get wrong value when key not exist")
+	}
+}
