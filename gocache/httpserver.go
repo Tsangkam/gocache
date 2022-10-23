d7158b8f2f54dde9c6dfd5ca7743b733cf5b82a3
Tsangkam <40147838+Tsangkam@users.noreply.github.com>
2022-10-23 19:34:21 +0800

    distributed-node complete, rearchitect project (#4)

--- gocache/httpserver.go
+++ gocache/httpserver.go
@@ -1,18 +1,25 @@
-package server
+package gocache
 
 import (
 	"fmt"
-	"gocache/gocache"
+	"gocache/gocache/consistenthash"
 	"log"
 	"net/http"
 	"strings"
+	"sync"
 )
 
-const defaultBasePath = "/_gocache/"
+const (
+	defaultBasePath = "/_gocache/"
+	defaultReplicas = 50
+)
 
 type HTTPPool struct {
-	self     string
-	basePath string
+	self       string
+	basePath   string
+	mu         sync.Mutex
+	peers      *consistenthash.Map
+	httpGetter map[string]*httpGetter
 }
 
 func NewHTTPPool(self string) *HTTPPool {
@@ -42,7 +49,7 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	groupName := parts[0]
 	key := parts[1]
 
-	group := gocache.GetGroup(groupName)
+	group := GetGroup(groupName)
 	if group == nil {
 		http.Error(w, "no such grouop: "+groupName, http.StatusNotFound)
 		return
@@ -57,3 +64,26 @@ func (p *HTTPPool) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Conten-Type", "application/octet-stream")
 	w.Write(view.ByteSlice())
 }
+
+func (p *HTTPPool) Set(peers ...string) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.peers = consistenthash.New(defaultReplicas, nil)
+	p.peers.Add(peers...)
+	p.httpGetter = make(map[string]*httpGetter, len(peers))
+	for _, peer := range peers {
+		p.httpGetter[peer] = &httpGetter{baseURL: peer + p.basePath}
+	}
+}
+
+func (p *HTTPPool) PickPeer(key string) (PeerGetter, bool) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	if peer := p.peers.Get(key); peer != "" && peer != p.self {
+		p.Log("Pick peer %s", peer)
+		return p.httpGetter[peer], true
+	}
+	return nil, false
+}
+
+var _ PeerPicker = (*HTTPPool)(nil) // detect HTTPPool implement PeerPicker's method
--- main.go
+++ main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"gocache/gocache"
-	"gocache/server"
 	"log"
 	"net/http"
 )
@@ -15,9 +15,8 @@ var db = map[string]string{
 	"Sam":  "567",
 }
 
-func main() {
-	// test data
-	gocache.NewGroup("scores", 2<<10, gocache.GetterFunc(
+func createGroup() *gocache.Group {
+	return gocache.NewGroup("scores", 2<<10, gocache.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
 			if v, ok := db[key]; ok {
@@ -25,10 +24,54 @@ func main() {
 			}
 			return nil, fmt.Errorf("%s not exist", key)
 		}))
+}
 
-	addr := "localhost:9999"
-	peers := server.NewHTTPPool(addr)
-	log.Println("gocache is runing at", addr)
-	log.Fatal(http.ListenAndServe(addr, peers))
+func startCacheServer(addr string, addrs []string, gogroup *gocache.Group) {
+	peers := gocache.NewHTTPPool(addr)
+	peers.Set(addrs...)
+	gogroup.RegisterPeers(peers)
+	log.Println("geecache is runing at", addr)
+	log.Fatal(http.ListenAndServe(addr[7:], peers))
+}
 
+func startAPIServer(apiAddr string, gogroup *gocache.Group) {
+	http.Handle("/api", http.HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			key := r.URL.Query().Get("key")
+			view, err := gogroup.Get(key)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Header().Set("Content-Type", "application/octet-stream")
+			w.Write(view.ByteSlice())
+		}))
+	log.Println("fontend server is runing at", apiAddr)
+	log.Fatal(http.ListenAndServe(apiAddr[7:], nil))
+}
+
+func main() {
+	var port int
+	var api bool
+	flag.IntVar(&port, "port", 8001, "gocache server port")
+	flag.BoolVar(&api, "api", false, "Start a api server?")
+	flag.Parse()
+
+	apiAddr := "http://localhost:9999"
+	addrMap := map[int]string{
+		8001: "http://localhost:8001",
+		8002: "http://localhost:8002",
+		8003: "http://localhost:8003",
+	}
+
+	var addrs []string
+	for _, v := range addrMap {
+		addrs = append(addrs, v)
+	}
+
+	gogroup := createGroup()
+	if api {
+		go startAPIServer(apiAddr, gogroup)
+	}
+	startCacheServer(addrMap[port], []string(addrs), gogroup)
 }
