49c62a34d9225217df7fa3b93c84c32e2e3500b1
Tsangkam <40147838+Tsangkam@users.noreply.github.com>
2022-10-26 02:22:12 +0800

    protobuf complete (#6)

--- gocache/client.go
+++ gocache/client.go
@@ -2,38 +2,45 @@ package gocache
 
 import (
 	"fmt"
+	pb "gocache/gocachepb"
 	"io"
 	"net/http"
 	"net/url"
+
+	"google.golang.org/protobuf/proto"
 )
 
 type httpGetter struct {
 	baseURL string
 }
 
-func (h *httpGetter) Get(group string, key string) ([]byte, error) {
+func (h *httpGetter) Get(in *pb.Request, out *pb.Response) error {
 	u := fmt.Sprintf(
 		"%v%v/%v",
 		h.baseURL,
-		url.QueryEscape(group),
-		url.QueryEscape(key),
+		url.QueryEscape(in.GetGroup()),
+		url.QueryEscape(in.GetKey()),
 	)
 	res, err := http.Get(u)
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("server returned: %v", res.Status)
+		return fmt.Errorf("server returned: %v", res.Status)
 	}
 
 	bytes, err := io.ReadAll(res.Body)
 	if err != nil {
-		return nil, fmt.Errorf("reading response body: %v", err)
+		return fmt.Errorf("reading response body: %v", err)
 	}
 
-	return bytes, nil
+	if err = proto.Unmarshal(bytes, out); err != nil {
+		return fmt.Errorf("decoding response body: %v", err)
+	}
+
+	return nil
 }
 
 var _ PeerGetter = (*httpGetter)(nil) // detect httpGetter implement PeerGetter's
